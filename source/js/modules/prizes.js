2cc0f6c9f2460d5e32126bc1691169e923ded39a
Andrey Ivanov <andrey-tm203@mail.ru>
2021-03-18 22:56:12 +0300

    implement second prize animation

--- source/js/modules/prizes.js
+++ source/js/modules/prizes.js
@@ -1,5 +1,37 @@
-const paths = [
-  `img/module-3/img/primary-award-from.svg`
+const animationsData = [
+  {path: `img/module-3/img/primary-award-from.svg`, duration: 6250},
+  {path: `img/module-3/img/secondary-award-from.svg`, duration: 2017},
+  {path: `img/prize3.svg`, duration: 0},
 ];
 
-export {paths};
+class PrizesAnimationsManager {
+  constructor() {
+    this.screen = document.querySelector(`#prizes`);
+    this.wasVisitedPage = false;
+  }
+
+  async animate() {
+    if (this.wasVisitedPage) {
+      return;
+    }
+
+    this.wasVisitedPage = true;
+
+    const prizes = [...this.screen.querySelectorAll(`.prizes__item`)];
+    const pictures = prizes.map((prize) => prize.querySelector(`img`));
+
+    for (let i = 0; i < pictures.length; i++) {
+      pictures[i].src = this.getRandomPath(animationsData[i].path);
+
+      await new Promise((resolve) => {
+        setTimeout(resolve, animationsData[i].duration);
+      });
+    }
+  }
+
+  getRandomPath(path) {
+    return `${path}?${Math.floor(Math.random() * 1000000000)}`;
+  }
+}
+
+export {PrizesAnimationsManager};
